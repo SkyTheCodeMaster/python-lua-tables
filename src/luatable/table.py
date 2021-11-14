4a8cca71605994372090e69642b41008f459204c
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2021-11-14 13:14:32 -0500

    Fix loading dicts that hold dicts

--- src/luatable/table.py
+++ src/luatable/table.py
@@ -1,10 +1,12 @@
 class Table(dict):
+  def __init__(self,d:dict=None):
+    if d:
+      for k,v in d.items():
+        self.__setattr__(k,v)
   def __getattr__(self,x):
     return self.get(x,None)
-  
   def __getitem__(self,x):
     return self.get(x,None)
-  
   def __setattr__(self,k,v):
     if type(v) is dict:
       temp = Table()
